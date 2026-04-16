d3834cad92313920d08a773735ad4d47ddfaa083
w3namsan-art <w3namsan@gmail.com>
2026-04-16 10:59:19 -0400

    Delete animebuilder directory

--- animebuilder/lib/wagmi.ts
+++
@@ -1,9 +0,0 @@
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { animeChainTestnet } from "./chains";
-
-export const wagmiConfig = getDefaultConfig({
-  appName: "AnimeBuilder",
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "YOUR_WALLETCONNECT_PROJECT_ID",
-  chains: [animeChainTestnet],
-  ssr: true,
-});
